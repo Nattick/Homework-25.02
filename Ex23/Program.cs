77706711198776fba896bc8232e624029b7a4347
Nattick <natalia.a.konovalova@gmail.com>
2023-02-22 10:19:36 +0300

    Add solution 23

---
+++ Ex23/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int N();
+{
+    Console.WriteLine("Enter any number:");
+    int n = Convert.ToInt32(Console.Readline());
+    return n;
+}
+void Cube(int number)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        int cube i * i * i;
+        Console.Write($"cube ");
+    }
+}
+int n  = N();
+Console.Writeline($"1 to {n} cube:  ");
+Cube(n);
